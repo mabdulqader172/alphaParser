d49da540a9b2067831ba54ec0fba98e1e88a362d
mabdulqader172 <85646507+mabdulqader172@users.noreply.github.com>
2022-07-17 19:57:45 -0700

    Updated Requirements + setup.py

---
+++ README.md
@@ -0,0 +1,6 @@
+# alphaParser
+`alphaParser` is a python software that parses AlphaFold results into a PyMOL `.pse` file of the best predicted model 
+and a `.png` visualization of the Predicted Alignment Error matrix. More details relative to the `.pse` and `.png` files
+will be explained below.
+
+## Installing `alphaParser`
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='alphaParser',
+    version='0.1dev',
+    packages=['alphaParser'],
+    url='https://github.com/mabdulqader172/alphaParser',
+    license='MIT License',
+    author='Mohammad Abdulqader',
+    author_email='m.abdulqader172@gmail.com',
+    description='A python software to parse AlphaFold results into PyMOL PSE and PAE plots.'
+)
